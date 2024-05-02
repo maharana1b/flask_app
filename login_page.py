5cf3295aacdcbad33991c7e25ea8b40332b9fde0
maharana1b <maharana.biki95@gmail.com>
2024-05-02 15:47:51 +0530

    first render

--- login_page.py
+++ login_page.py
@@ -2,14 +2,17 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route('/login', methods=["POST"])
+@app.route('/login',methods=["GET","POST"])
 def login():
-    user_name = request.form.get('user_name')
-    password = request.form.get('pass_word')
-    if user_name == "kore" and password == "mphasis":
-        return "Welcome {}".format(user_name)
-    else:
-        return "Invalid credentials. Please try again."
+    if request.method == "POST":
+        user_name = request.form.get('user_name')
+        password = request.form.get('pass_word')
+        print(user_name,password)
+        if user_name == "kore" and password == "mphasis":
+            return "Welcome {}".format(user_name)
+        else:
+            return "Invalid credentials. Please try again."
+    return render_template('login.html',user_name=user_name)
 
     
 
